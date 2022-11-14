80e907e966bc9fa983b1f7da8515816417c23ebb
Iñigo Escolar <inigo@MacBook-Pro-de-Inigo.local>
2022-11-14 20:41:02 +0100

    added automatic saving when opening tree

--- lua/Inigo/core/keymaps.lua
+++ lua/Inigo/core/keymaps.lua
@@ -4,31 +4,38 @@ local keymap = vim.keymap
 -- general keymaps
 
 keymap.set("i", "jk", "<ESC>")
-keymap.set("n", "<leader>h",":nohl<CR>")
-keymap.set("n", "x", '"_x') 
+keymap.set("n", "<leader>h", ":nohl<CR>")
+keymap.set("n", "x", '"_x')
 
-keymap.set("n", "<leader>+","<C-a>")
+keymap.set("n", "<leader>+", "<C-a>")
 keymap.set("n", "<leader>-", "<C-x>")
 -- windows
 keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
-keymap.set("n","<leader>sh", "<C-W>S") -- split window horizontally
-keymap.set("n","<leader>se", "<C-") -- make split windows equal W
-keymap.set("n","<leader>sx", ":close<CR>") -- close current split wi
+keymap.set("n", "<leader>sh", "<C-W>S") -- split window horizontally
+keymap.set("n", "<leader>se", "<C-") -- make split windows equal W
+keymap.set("n", "<leader>sx", ":close<CR>") -- close current split wi
 --tabs
 keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
-keymap.set("n","<leader>tx", ":tabclose<CR>") -- close current tab
-keymap.set("n","<leader>tn", ": tabn<CR>") -- go to next tab
-keymap.set("n","<leader>tp", ": tabp<CR>") -- go to previous tab
-
+keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
+keymap.set("n", "<leader>tn", ": tabn<CR>") -- go to next tab
+keymap.set("n", "<leader>tp", ": tabp<CR>") -- go to previous tab
 
 -- vim-maximizer
 keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>") -- toggle split window maximization
 
 -- nvimtree
+-- keymap.set("n", "<leader>et", function()
+-- 	vim.cmd(":w")
+-- 	vim.cmd(":NvimTreeToggle")
+-- end)
+--
 keymap.set("n", "<leader>et", ":NvimTreeToggle<CR>")
-keymap.set("n", "<leader>ef", ":NvimTreeFocus<CR>")
-keymap.set("n", "<leader>ev", "<C-v>") -- open while splitting vertically
-keymap.set("n", "<leader>eh", "<C-h>") -- open while splitting horizontally
+keymap.set("n", "<leader>ef", function()
+	vim.cmd(":w")
+	vim.cmd(":NvimTreeFocus")
+end)
+
+-- keymap.set("n", "<leader>eh", "<C-h>") -- open while splitting horizontally
 
 -- telescope
 keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
@@ -36,25 +43,3 @@ keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in 
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
 keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
